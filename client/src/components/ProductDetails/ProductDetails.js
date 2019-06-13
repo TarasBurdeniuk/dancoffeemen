9b254a51d1df684aef95d16955fa08c513bcf403
Taras Shumeiko <shumeikotaras1@gmail.com>
2019-06-13 20:48:10 +0300

    quantity

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -7,10 +7,44 @@ import Grid from '@material-ui/core/Grid';
 const useStyles = makeStyles({
 	container: {
 		marginTop: '2rem'
+	},
+	availability: {
+		display: 'inline-block'
+	},
+	quantity: {
+		display: 'inline-block',
+		borderRadius: '3px',
+		boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
+	},
+	quantityInput: {
+		width: '4rem',
+		padding: '.7rem',
+		fontSize: '1.5rem',
+		textAlign: 'center',
+		border: 0,
+		background: '#fff'
+	},
+	quantityChange: {
+		width: '3rem',
+		padding: '.7rem',
+		textAlign: 'center',
+		textShadow: '0 1px 0 rgba(#fff, .6)',
+		fontSize: '1.5rem',
+		border: '0 solid #dbdbdb',
+		outline: 'none',
+		background: '#f3f3f3',
+		color: '#888',
+		cursor: 'pointer',
+		'&:hover': {
+			background: '#e3e3e3',
+			color: '#636363'
+		}
 	}
 });
 
-const ProductDetails = () => {
+const ProductDetails = (props) => {
+	const { increment, decrement, value } = props;
+
 	const classes = useStyles();
 
 	return (
@@ -20,27 +54,39 @@ const ProductDetails = () => {
 					<img src={require('./temporaryPictures/coffee-package-1.jpg')} alt="coffee-package" />
 				</Grid>
 				<Grid item xs={12} md={7}>
-					<Typography variant="h5" component="h2" gutterBottom>
+					<Typography variant="h4" component="h2" gutterBottom>
 						Lavazza Pienaroma
 					</Typography>
 					<Typography variant="subtitle1" gutterBottom>
 						Rate
 					</Typography>
-					<Typography variant="subtitle1" gutterBottom>
-						Price
+					<Typography variant="h6" gutterBottom>
+						$18
 					</Typography>
 					<Typography variant="subtitle1" gutterBottom>
-						Short description
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+						A adipisci atque consequatur consequuntur, corporis cupiditate
+						distinctio dolore enim fugit iusto labore laborum magni maiores
+						molestiae, odit quisquam sapiente tempora. Impedit!
 					</Typography>
-					<Typography variant="subtitle1" gutterBottom>
-						Availability
-					</Typography>
-					<Typography variant="subtitle1" gutterBottom>
-						Quantity
+					<Typography variant="h6" gutterBottom>
+						Availability:&nbsp;
+						<Typography className={classes.availability}>
+							In Stock
+						</Typography>
 					</Typography>
 					<Typography variant="subtitle1" gutterBottom>
 						Size
 					</Typography>
+					<div className={classes.quantity}>
+						<button className={classes.quantityChange} onClick={decrement}>
+							&mdash;
+						</button>
+						<input className={classes.quantityInput} type="text" value={value} disabled />
+						<button className={classes.quantityChange} onClick={increment}>
+							&#xff0b;
+						</button>
+					</div>
 					<Typography variant="subtitle1" gutterBottom>
 						Add to cart
 					</Typography>
@@ -49,6 +95,10 @@ const ProductDetails = () => {
 			<Grid item xs={12}>
 				<Typography variant="h5" component="h2" gutterBottom>
 					Description
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores aspernatur autem consequuntur cupiditate, delectus esse illum iste itaque maxime odio pariatur praesentium, quae, quas quisquam soluta totam unde vitae!
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda distinctio ducimus excepturi ipsa iste laborum magni maxime minima nisi odit officiis perspiciatis quis, recusandae repudiandae, totam ullam, voluptas voluptatum!
+					</Typography>
 				</Typography>
 			</Grid>
 		</Container>
--- client/src/components/ProductDetails/index.js
+++ client/src/components/ProductDetails/index.js
@@ -3,12 +3,28 @@ import ProductDetails from './ProductDetails';
 
 class ProductDetailsContainer extends Component {
 	state = {
+		value: 1
+	};
 
+	increment = () => {
+		this.setState({value: this.state.value + 1})
+	};
+
+	decrement = () => {
+		if (this.state.value > 0) {
+			this.setState({value: this.state.value - 1})
+		}
 	};
 
 	render() {
+		const { value } = this.state;
+
 		return (
-			<ProductDetails />
+			<ProductDetails
+				increment={this.increment}
+				decrement={this.decrement}
+				value={value}
+			/>
 		);
 	}
 }
