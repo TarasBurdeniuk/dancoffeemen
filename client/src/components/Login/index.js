9963d6a5633f623685d5f9fae2ca9b1e878eaf41
Taras Burdeniuk <37422333+TarasBurdeniuk@users.noreply.github.com>
2019-06-20 18:32:40 +0300

    added forms login and register (#15)

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,7 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const Login = () => {
-	return <div>Login</div>;
+const useStyles = makeStyles(theme => ({
+	'@global': {
+		body: {
+			backgroundColor: theme.palette.common.white,
+		},
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+	container: {
+		minHeight: 'calc(100vh - 213px)',
+	},
+}));
+
+const SignIn = () => {
+	const classes = useStyles();
+
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const { email, password } = formData;
+
+	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = async e => {
+		e.preventDefault();
+		if (!email || !password) {
+			// instead console must be error modal window todo
+			return console.error('Enter email or password');
+		}
+		// instead console must be function to fetch todo
+		console.log({ email, password });
+	};
+
+	return (
+		<Container className={classes.container} component="main" maxWidth="xs">
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<form className={classes.form} noValidate onSubmit={e => onSubmit(e)}>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						value={email}
+						onChange={e => onChange(e)}
+						autoFocus
+					/>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						value={password}
+						onChange={e => onChange(e)}
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						className={classes.submit}
+					>
+						Sign In
+					</Button>
+					<Grid container justify="flex-end">
+						<Grid item>
+							<Link to="/register" variant="body2">
+								Don't have an account? Sign Up
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+		</Container>
+	);
 };
 
-export default Login;
+export default SignIn;
--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,7 +1,154 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const Register = () => {
-	return <div>Register</div>;
+const useStyles = makeStyles(theme => ({
+	'@global': {
+		body: {
+			backgroundColor: theme.palette.common.white,
+		},
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(3),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+	container: {
+		minHeight: 'calc(100vh - 213px)',
+	},
+}));
+
+const SignUp = () => {
+	const classes = useStyles();
+
+	const [formData, setFormData] = useState({
+		name: '',
+		phone: '',
+		email: '',
+		password: '',
+		password2: '',
+	});
+
+	const { name, phone, email, password, password2 } = formData;
+
+	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = async e => {
+		e.preventDefault();
+		if (password !== password2 || !name || !email || !phone) {
+			// instead console must be error modal window todo
+			console.error('All fields must be filled in');
+		} else {
+			// instead console must be function to fetch todo
+			console.log({ name, phone, email, password });
+		}
+	};
+
+	return (
+		<Container className={classes.container} component="main" maxWidth="xs">
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Typography component="h1" variant="h5">
+					Sign up
+				</Typography>
+				<form className={classes.form} noValidate onSubmit={e => onSubmit(e)}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								label="Name"
+								name="name"
+								value={name}
+								onChange={e => onChange(e)}
+								autoFocus
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								value={email}
+								onChange={e => onChange(e)}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="phone"
+								label="Phone"
+								name="phone"
+								value={phone}
+								minLength="10"
+								onChange={e => onChange(e)}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								value={password}
+								onChange={e => onChange(e)}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								name="password2"
+								label="Repeat password"
+								type="password"
+								value={password2}
+								onChange={e => onChange(e)}
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						className={classes.submit}
+					>
+						Sign Up
+					</Button>
+					<Grid container justify="flex-end">
+						<Grid item>
+							<Link to="/login" variant="body2">
+								Already have an account? Sign in
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+		</Container>
+	);
 };
 
-export default Register;
+export default SignUp;
