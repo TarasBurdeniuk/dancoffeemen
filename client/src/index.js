0c2dfe392be6bfc8768d42749d3598d796bba355
Taras Burdeniuk <bur.taras@gmail.com>
2019-06-26 18:58:35 +0300

    add rootMaterialStyles; change some style in Login and Register forms

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import { ThemeProvider } from '@material-ui/styles';
+import { theme } from './styles/rootMaterialStyles';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<ThemeProvider theme={theme}>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</ThemeProvider>,
 	document.getElementById('root'),
 );
