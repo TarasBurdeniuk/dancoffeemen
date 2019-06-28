7ddc5a17f4b76ef2a7d6322bcf534ea632a35e62
Taras Burdeniuk <bur.taras@gmail.com>
2019-06-28 15:16:27 +0300

    fix some bugs in karusel

--- client/src/components/MainPage/KaruselMaterial/index.js
+++ client/src/components/MainPage/KaruselMaterial/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
@@ -6,25 +6,31 @@ import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import grey from '@material-ui/core/colors/grey';
+
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const white = grey[50];
 
 const tutorialSteps = [
 	{
+		id: 1,
 		imgPath:
 			'https://www.shambhalaschool.org/wp-content/uploads/2017/11/pp-hot-coffee-rf-istock.jpg',
 	},
 	{
+		id: 2,
 		imgPath: 'https://kabar.kg/site/assets/files/38765/1-1.jpg',
 	},
 	{
+		id: 3,
 		imgPath: 'https://riabir.ru/wp-content/uploads/2019/03/CHay.jpg',
 	},
 	{
+		id: 4,
 		imgPath: 'https://www.stanforddaily.com/wp-content/uploads/2016/05/coffee-grind.jpg',
 	},
 	{
+		id: 5,
 		imgPath:
 			'https://s3.amazonaws.com/bncore/wp-content/uploads/2018/05/1010290878-Gusto-Cheap-12-e1545847595185.jpg',
 	},
@@ -94,39 +100,37 @@ const SwipeableTextMobileStepper = () => {
 				enableMouseEvents
 			>
 				{tutorialSteps.map((step, index) => (
-					<Fragment>
-						<div key={step.label} className={classes.customDiv}>
-							<Button
-								size="large"
-								onClick={handleBack}
-								disabled={activeStep === 0}
-								className={classes.customButtonLeft}
-							>
-								{theme.direction === 'rtl' ? (
-									<KeyboardArrowRight />
-								) : (
-									<KeyboardArrowLeft />
-								)}
-								Back
-							</Button>
-							{Math.abs(activeStep - index) <= 2 ? (
-								<img className={classes.img} src={step.imgPath} alt={step.label} />
-							) : null}
-							<Button
-								size="large"
-								onClick={handleNext}
-								disabled={activeStep === maxSteps - 1}
-								className={classes.customButtonRight}
-							>
-								Next
-								{theme.direction === 'rtl' ? (
-									<KeyboardArrowLeft />
-								) : (
-									<KeyboardArrowRight />
-								)}
-							</Button>
-						</div>
-					</Fragment>
+					<div key={step.id} className={classes.customDiv}>
+						<Button
+							key={step.imgPath.slice(0, 10)}
+							size="large"
+							onClick={handleBack}
+							disabled={activeStep === 0}
+							className={classes.customButtonLeft}
+						>
+							{theme.direction === 'rtl' ? (
+								<KeyboardArrowRight />
+							) : (
+								<KeyboardArrowLeft />
+							)}
+						</Button>
+						{Math.abs(activeStep - index) <= 2 ? (
+							<img className={classes.img} src={step.imgPath} alt={step.label} />
+						) : null}
+						<Button
+							key={step.imgPath.slice(10, 15)}
+							size="large"
+							onClick={handleNext}
+							disabled={activeStep === maxSteps - 1}
+							className={classes.customButtonRight}
+						>
+							{theme.direction === 'rtl' ? (
+								<KeyboardArrowLeft />
+							) : (
+								<KeyboardArrowRight />
+							)}
+						</Button>
+					</div>
 				))}
 			</AutoPlaySwipeableViews>
 		</div>
